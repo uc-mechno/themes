62762ddb79d30766397ca8a41a4676a9464eb536
mechno <mechnotechno@gmail.com>
2024-07-06 17:57:01 +0900

    5-8完了

--- mytheme4/functions.php
+++ mytheme4/functions.php
@@ -20,6 +20,10 @@ function mytheme_setup() {
 		'primary' => 'メイン',
 	) );
 
+  	// 編集画面用のCSS
+	add_theme_support( 'editor-styles' );
+	add_editor_style( 'editor-style.css' );
+
 }
 add_action( 'after_setup_theme', 'mytheme_setup' );
 
